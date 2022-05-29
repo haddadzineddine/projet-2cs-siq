f64bc7a8d02dcbce5ec7c6b9eb98f7f539a2616e
HADDAD Zineddine <hz_haddad@esi.dz>
2022-05-29 19:02:58 +0200

    move the login to the server side

--- script/main.py
+++ script/main.py
@@ -1,50 +1,29 @@
 import typer
 import yaml
 import socket
+import requests
+import json
 
-user = {
-    "name": "John",
-    "token": "12345"
-}
 
 IP = socket.gethostbyname(socket.gethostname())
 
+apiUrl = "http://127.0.0.1:8000"
 
-class Deployment:
-    def __init__(self, name, label, image, ip):
-        self.name = name
-        self.label = label
-        self.image = image
-        self.ip = ip
-
-    def __getDeploymentTemplate():
-        with open("templates/deployment.yaml") as f:
-            return yaml.load(f, Loader=yaml.FullLoader)
-
-    def __generateDeployment(self):
-        template = Deployment.__getDeploymentTemplate()
-        template["metadata"]["name"] = self.name
-        template['metadata']['labels']['app'] = self.label
-        template['spec']['selector']['matchLabels']['app'] = self.label
-        template['spec']['template']['metadata']['labels']['app'] = self.label
-        template["spec"]["template"]["spec"]["containers"][0]["image"] = self.image
-        template['spec']['template']['spec']['containers'][0]['name'] = self.name + '-conatiner'
-        template['spec']['template']['spec']['containers'][0]['env'].append({
-            "name": "DISPLAY",
-            "value": self.ip + ":0.0"
-        })
-        return template
-
-    def export(self):
-        with open("generators/deployment.yaml", "w") as f:
-            yaml.dump(self.__generateDeployment(), f)
 
 
 def main():
-    deployment = Deployment(
-        user['name'], user['name'], 'haddadzineddine/packet-tracer', IP)
 
-    deployment.export()
+    credentials = {
+        "email": "hz_haddad@esi.dz",
+        "password": "12345678"
+    }
+
+    response = requests.post(apiUrl + '/login', data=json.dumps(credentials))
+    user = response.json()
+
+    print(user)
+
+   
 
 
 if __name__ == "__main__":
--- server/server.py
+++ server/server.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 from pymongo import MongoClient
 import bcrypt
+import os
+import yaml
+
 app = FastAPI()
 
 
@@ -24,6 +27,43 @@ class Register(BaseModel):
     password: str
 
 
+class Deployment:
+    def __init__(self, name, label, image, ip):
+        self.name = name
+        self.label = label
+        self.image = image
+        self.ip = ip
+
+    def __getDeploymentTemplate():
+        with open("templates/deployment.yaml") as f:
+            return yaml.load(f, Loader=yaml.FullLoader)
+
+    def __generateDeployment(self):
+        template = Deployment.__getDeploymentTemplate()
+        template["metadata"]["name"] = self.name
+        template['metadata']['labels']['app'] = self.label
+        template['spec']['selector']['matchLabels']['app'] = self.label
+        template['spec']['template']['metadata']['labels']['app'] = self.label
+        template["spec"]["template"]["spec"]["containers"][0]["image"] = self.image
+        template['spec']['template']['spec']['containers'][0]['name'] = self.name + '-conatiner'
+        template['spec']['template']['spec']['containers'][0]['env'].append({
+            "name": "DISPLAY",
+            "value": self.ip + ":0.0"
+        })
+        return template
+
+    def export(self):
+        with open("generators/deployment.yaml", "w") as f:
+            yaml.dump(self.__generateDeployment(), f)
+
+
+class DeploymentModel(BaseModel):
+    name: str
+    label: str
+    image: str
+    ip: str
+
+
 @app.post("/login", status_code=status.HTTP_200_OK)
 def login(login: Login, response: Response):
     collection = client['packet-tracer'].users
@@ -41,3 +81,15 @@ def register(register: Register):
     user = collection.insert_one(register.__dict__)
     if not user:
         response.status_code = status.HTTP_400_BAD_REQUEST
+
+
+@app.get('/databases')
+def get_databases():
+    return client.list_database_names()
+
+
+@app.post('/run-deployment')
+def run_packet_tracer(deploymentModel: DeploymentModel):
+    deployment = Deployment(deploymentModel.name, deploymentModel.label, deploymentModel.image, deploymentModel.ip)
+    deployment.export()
+    os.system("kubectl apply -f generators/deployment.yaml") 
